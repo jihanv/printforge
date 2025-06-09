5cbd81b3414e68168c32b1c21f5b1bb32c8b5159
jihanv <hedjmny@gmail.com>
2025-06-09 20:14:28 +0900

    Add logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import "./globals.css";
 
+/**
+ * Challenge: Add the header to the project
+ * 
+ * Hint: for the printforge logo(s), use a string src on the `<img />`:
+ * src="/printforge-logo.svg" (Desktop logo)
+ * src="/printforge-logo-icon.svg" (Mobile logo)
+ */
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,6 +16,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              {/* Desktop logo */}
+              <img
+                src="/printforge-logo.svg"
+                alt="PrintForge Logo"
+                className="w-[200px] h-auto hidden md:block"
+              />
+              {/* Mobile logo */}
+              <img
+                src="/printforge-logo-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden"
+              />
+            </div>
+            <ul className="flex items-center gap-2.5">
+              <p>3D Models</p>
+              <p>About</p>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
