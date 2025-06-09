1decb17e74fc97c11d8c636ecc7c9d4f28e5abcc
jihanv <hedjmny@gmail.com>
2025-06-09 20:56:43 +0900

    Optimize image and font

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,34 @@
+import HeroImage from "@/public/hero-image.png"
+
 export default function Home() {
   return (
     <main>
-      <section className="flex flex-col-reverse md:flex-row items-center justify-between gap-12 px-6 py-16 mx-auto max-w-7xl">
-        <div className="flex-1 space-y-6 text-center md:text-left">
-          <p className="text-sm text-gray-600 uppercase tracking-wide">
+      <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl">
+        <div className="flex-1 space-y-6">
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
             Your go-to platform for 3D printing files
           </p>
-          <h1 className="text-4xl md:text-5xl font-bold text-black leading-tight">
-            Discover what’s possible with 3D printing
+          <h1 className="text-4xl font-bold md:text-5xl">
+            Discover what's possible with 3D Printing
           </h1>
-          <p className="text-lg text-gray-700">
-            Join our community of creators and explore a vast library of user-submitted models.
+          <p className="text-lg text-gray-600">
+            Join our community of creators and explore a vast
+            library of user-submitted models.
           </p>
-          <div className="mt-4">
-            <a
+
+          <div className="flex gap-4">
+            <button
               href="/3d-models"
-              className="inline-block px-6 py-3 border-2 border-black text-black font-medium transition duration-200 hover:bg-black hover:text-white"
+              className="px-6 py-3 text-black transition duration-100 bg-white border-2 border-black hover:bg-black hover:text-white"
             >
               Browse Models
-            </a>
+            </button>
           </div>
         </div>
 
-        <div className="flex-1 flex justify-center">
-          <img
-            src="/hero-image.png"
-            alt="3D printing graphic"
-            className="w-[350px] h-auto"
-          />
-        </div>
+        <img src={HeroImage.src} className="w-[350px] h-auto rounded-lg" />
+
       </section>
     </main>
-  );
+  )
 }
