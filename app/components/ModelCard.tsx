387339d12957890f970ce16ed6cf29c596fcd862
jihanv <hedjmny@gmail.com>
2025-06-28 19:44:30 +0900

    Add category route

--- app/components/ModelCard.tsx
+++ app/components/ModelCard.tsx
@@ -3,6 +3,8 @@ import { FaRegHeart } from "react-icons/fa6"
 import Pill from "./Pill"
 import { ModelCardProps } from "@/app/types"
 import placeholderImg from "@/public/placeholder.png"
+import Image from "next/image"
+
 
 export default function ModelCard({ model }: ModelCardProps) {
     return (
@@ -13,10 +15,12 @@ export default function ModelCard({ model }: ModelCardProps) {
         >
             <div className="overflow-hidden transition-shadow bg-white rounded-lg shadow-md hover:shadow-lg" role="article">
                 <div className="relative aspect-square">
-                    <img
+                    <Image
                         src={placeholderImg.src}
                         alt={model.name}
                         className="absolute inset-0 object-cover w-full h-full"
+                        width={300}
+                        height={300}
                     />
                 </div>
                 <div className="p-4">
--- app/types/index.ts
+++ app/types/index.ts
@@ -21,6 +21,12 @@ export type CategoriesData = {
 }
 
 // Page Types
+export type CategoryPageProps = {
+    params: Promise<{
+        categoryName: string
+    }>
+}
+
 export type RootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>
